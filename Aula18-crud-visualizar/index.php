4a4fd9eac18afe8baaeec6d698b2ba54db7665bb
joaogutierrre <gutierrejoao@gmail.com>
2020-07-08 18:37:23 -0300

    Aula 18, visualizando usuários de forma individual

---
+++ Aula18-crud-visualizar/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Gutierre</title>
+</head>
+
+<body>
+	<h1>Listar Usuário</h1>
+	<?php
+	require 'Conn.php';
+	$conn = new Conn();
+
+	$result_user = "SELECT * FROM gutierre.usuarios";
+
+	$resultado_user = $conn->getConn()->prepare($result_user);
+	$resultado_user->bindParam(':nome', $nome, PDO::PARAM_STR);
+	$resultado_user->execute();
+
+	while ($row_user = $resultado_user->fetch(PDO::FETCH_ASSOC)) :
+		// var_dump($row_user);
+		echo "ID: " . $row_user['id'] . "<br>";
+		echo "Nome User: " . $row_user['nome'] . "<br>";
+		echo "Email: " . $row_user['email'] . "<br>";
+		echo "Usuario: " . $row_user['usuario'] . "<br>";
+		echo "Foto: " . $row_user['foto'] . "<br>";
+		echo "Criado em: " . date('d/m/Y H:i:s', strtotime($row_user['created'])) . "<br>";
+
+		if (!empty($row_user['modified'])) :
+			echo "Ultima alteração em: " . date('d/m/Y H:i:s', strtotime($row_user['modified'])) . "<br>";
+		else :
+			echo "Ainda não houveram alterações<br>";
+		endif;
+
+		echo "<a href='visualizar.php?id=".$row_user['id']."'>Visualizar</a><br><hr>";
+
+
+	endwhile;
+
+
+
+
+	?>
+
+</body>
+
+</html>
