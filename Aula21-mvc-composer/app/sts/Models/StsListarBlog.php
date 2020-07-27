3dc73c4453c86e5c263906e741107809b5dfadf9
joaogutierrre <gutierrejoao@gmail.com>
2020-07-27 18:39:50 -0300

    Começando a criação de uma das models da aplicação.

---
+++ Aula21-mvc-composer/app/sts/Models/StsListarBlog.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sts\Models;
+/**
+ * 
+ * 
+ * @copyright (c) 2020, João Gutierre.
+ * 
+ */
+
+ class StsListarBlog{
+    public function listar(){
+        //echo "Pesquisa arquivos no bd<br>";
+
+        
+    }
+ }
